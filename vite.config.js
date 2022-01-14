e0cddf6d4043ec5859b903c94741785f74a9f1ab
Ron <35429278+enjinerd@users.noreply.github.com>
2022-01-14 13:18:23 +0700

    chore(app): :technologist: add husky and absolute path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { readdirSync } from "fs";
+import path from "path";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+// https://stackoverflow.com/questions/69424422/use-compileroptions-baseurl-with-vite-js-project
+const absolutePathAliases = {};
+// Root resources folder
+const srcPath = path.resolve("./src/");
+// Ajust the regex here to include .vue, .js, .jsx, etc.. files from the resources/ folder
+const srcRootContent = readdirSync(srcPath, {
+  withFileTypes: true,
+}).map((dirent) => dirent.name.replace(/(\.js){1}(x?)/, ""));
+
+srcRootContent.forEach((directory) => {
+  absolutePathAliases[directory] = path.join(srcPath, directory);
+});
+
 export default defineConfig({
-  plugins: [react()]
-})
+  alias: {
+    ...absolutePathAliases,
+  },
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
+});
